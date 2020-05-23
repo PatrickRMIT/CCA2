99bf13b153ccb1efd10a45471e7740c9575fbb14
PatrickRMIT <61533573+PatrickRMIT@users.noreply.github.com>
2020-05-24 01:24:58 +1000

    Add files via upload

--- maps.php
+++ maps.php
@@ -15,26 +15,86 @@ $user = $datastore->lookup($key);
 <!DOCTYPE html>
 <html>
   <head>
+    <meta charset="utf-8">
     <title>Simple Map</title>
-	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        width: 100%;
-			  height: 100%;
-		   	border: 1px solid blue;
-      }
-      /* Optional: Makes the sample page fill the window. */
       .container1 {
-			height: 450px;
-		}
+        height: 450px;
+      }
+
+      #map_canvas {
+        width: 100%;
+        height: 100%;
+        order: 1px solid blue;
+      
+      }
+
     </style>
-  </head>
-  <body>
-  <link type="text/css" rel="stylesheet" href="/stylesheets/main.css" />
-    <body>
+ </head>
+
+ <link type="text/css" rel="stylesheet" href="/stylesheets/main.css" />
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB0nu7HabL4z8Pzb_S3LRnv0m8GHQaUU5Q&callback=initMap"></script>
+
+    <script>
+      // The web service URL from Drive 'Deploy as web app' dialog.
+      var DATA_SERVICE_URL = "https://script.google.com/macros/s/AKfycbwa8jG2qlQSaffI90my88KDm-ZC9YTwDCh19G-0oVbj200rKBI/exec?jsonp=callback";
+
+      var map, infoWindow;
+
+      function initialize() {
+        map = new google.maps.Map(document.getElementById('map_canvas'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 12
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+
+
+        var scriptElement = document.createElement('script');
+        scriptElement.src = DATA_SERVICE_URL;
+        document.getElementsByTagName('head')[0].appendChild(scriptElement);
+      }
+
+      function callback(data) {
+        for (var i = 0; i < data.length; i++) {
+          var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(data[i][2], data[i][1]),
+            map: map
+          });
+
+        }
+        }
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+
+    </script>
+ 
+  <body onload="initialize()">
     <div class="header">
   <h1>WECLOME BACK <?php echo $user['name'];?></h1>
   <h2>View the trends</h2>
@@ -48,52 +108,10 @@ $user = $datastore->lookup($key);
   <li><a href="https://cca2patrickjoel.ts.r.appspot.com/profile">Profile</a></li>
 </ul>
 <div class="container1">
-    <div id="map"></div>
+    <div id="map_canvas"></div>
     </div>
-    <script>
-       var map, infoWindow;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 6
-        });
-        infoWindow = new google.maps.InfoWindow;
-
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Your Location ');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB0nu7HabL4z8Pzb_S3LRnv0m8GHQaUU5Q&callback=initMap"
-    async defer></script>
-
-    <div class="footer">
+  <div class="footer">
   <p>Developed By Patrick Jones S3661943 & Joel Jacob 3660851 for Cloud Computing COSC2626/2640 Assignment 2</p>
 </div>
-
-  </body>
+</body>
 </html>
